37f1ca97f78423fda7c8ca35a2325b547e15733a
Richard Carter <Richard-CRT@users.noreply.github.com>
2017-08-07 15:04:20 +0100

    Renamed form file

--- PERF-Design/MainForm.Designer.cs
+++ PERF-Design/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PERF_Design
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
--- PERF-Design/Program.cs
+++ PERF-Design/Program.cs
@@ -16,7 +16,7 @@ namespace PERF_Design
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
